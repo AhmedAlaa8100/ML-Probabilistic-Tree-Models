0240492953257e756674434c38ceb94967d8ff0d
A-agha2607 <154236961+A-agha2607@users.noreply.github.com>
2025-12-07 22:56:01 +0200

    Refactor DecisionTree implementation: enhance tree growth logic, add stopping criteria, and improve feature ranking methods.

--- Decision_Trees.py
+++ Decision_Trees.py
@@ -15,12 +15,12 @@ X_train, X_temp, y_train, y_temp = train_test_split(
     X, y, test_size=0.30, random_state=42, stratify=y
 )
 
-# Split the remaining 30% into 15% val and 15% test
-# 15% is half of 30% → test_size = 0.5
+
 X_val, X_test, y_val, y_test = train_test_split(
     X_temp, y_temp, test_size=0.5, random_state=42, stratify=y_temp
 )
 
+## Data class to hold the tree nodes
 class Node:
     def __init__(self, feature=None, threshold=None, left=None, right=None,*,value=None):
         self.feature = feature
@@ -35,6 +35,7 @@ class Node:
         return False
 
 
+## Model Implementation
 class DecisionTree:
     def __init__(self, min_samples_split=2, max_depth=2):
         self.min_samples_split=min_samples_split
@@ -44,19 +45,24 @@ class DecisionTree:
         
     def fit(self, X, y):
         self.all_gains = [0] * X.shape[1]
+        # recursively build the tree
         self.root = self._grow_tree(X, y)
         
     def _grow_tree(self, X, y, depth=0):
         n_samples, n_features = X.shape
-        n_labels = len(np.unique(y))
+        n_labels = len(np.unique(y))    
+        # stopping criteria
         if(depth>=self.max_depth or n_labels==1 or n_samples<self.min_samples_split):
             leaf_value = self._most_common_label(y)
             return Node(value=leaf_value)
+        # Determine the best split
         best_feature, best_threshold = self._best_split(X, y)
+        # mask data to given split
         left_mask=X[:, best_feature] <= best_threshold
         right_mask=X[:, best_feature] > best_threshold
         left_data, right_data = X[left_mask], X[right_mask]
         left_labels, right_labels = y[left_mask], y[right_mask]
+        # recursively grow the left and right children each with different data
         left_child=self._grow_tree(left_data, left_labels, depth+1)
         right_child=self._grow_tree(right_data, right_labels, depth+1)
         return Node(feature=best_feature, threshold=best_threshold, left=left_child, right=right_child)
@@ -75,7 +81,7 @@ class DecisionTree:
             values = np.sort(np.unique(X[:, feature]))
             thresholds = [(values[i] + values[i+1]) / 2 for i in range(len(values) - 1)]
 
-            best_feature_gain = -1  # reset best gain for THIS feature
+            best_feature_gain = -1  # reset best gain for this feature
 
             for threshold in thresholds:
                 gain = self._info_gain(y, X[:, feature], threshold)
@@ -108,15 +114,18 @@ class DecisionTree:
         weighted_entropy=(len(left_y)/len(y))*left_entropy + (len(right_y)/len(y))*right_entropy
         info_gain=parent_entropy - weighted_entropy
         return info_gain
+    
     def entropy(self, y):
         class_labels, counts = np.unique(y, return_counts=True)
         probabilities = counts / len(y)
         entropy = -np.sum(probabilities * np.log2(probabilities + 1e-9))
         return entropy
+    
     def _most_common_label(self, y):
         class_labels, counts = np.unique(y, return_counts=True)
         most_common = class_labels[np.argmax(counts)]
         return most_common
+    
     def predict(self, X):
         root=self.root
         predictions=[]
@@ -129,6 +138,7 @@ class DecisionTree:
                     node=node.right
             predictions.append(node.value)
         return np.array(predictions)
+    
     def ranked_features(self):
        
         if not self.all_gains:
